aa026572d1ad7445e5124bbc786091275b02f130
Mr Michael <michaellynch747@gmail.com>
2019-07-10 21:36:26 +0100

    Updates

---
+++ precache-manifest.ffb77b3eca110f34a8f4b4e826c78cef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275aeff6a103b29950f395d1defb0dfe",
+    "url": "/m-rolodex/index.html"
+  },
+  {
+    "revision": "1fd10bbfe7a59e5c988e",
+    "url": "/m-rolodex/static/css/main.240f1581.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/m-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1fd10bbfe7a59e5c988e",
+    "url": "/m-rolodex/static/js/main.4ec390a8.chunk.js"
+  },
+  {
+    "revision": "804794c80a8b1c5504f2",
+    "url": "/m-rolodex/static/js/runtime~main.7fe54dd8.js"
+  }
+]);
